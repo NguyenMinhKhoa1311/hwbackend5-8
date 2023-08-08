6b084c29bb94d341b7719a135baec5a3b15ef174
minhkhoa1311 <khoa.nm00716@sinhvien.hoasen.edu.vn>
2023-08-08 22:12:45 +0700

    update middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { AuthService } from './auth/auth.service';
+import { AuthMiddleware } from './auth/auth.middlware';
+
 @Module({
   imports: [ProductModule,MongooseModule.forRoot('mongodb+srv://map:13112002@cluster0.9cwrccp.mongodb.net/')],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    .apply(AuthMiddleware)
+    .forRoutes({path: '*', method: RequestMethod.ALL});
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
 
 async function bootstrap() {
+  const key = admin.initializeApp({
+    credential: admin.credential.cert('./homework5-8-key.json')
+  });
+  admin.messaging(key)
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
 }
